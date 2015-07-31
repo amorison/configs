a46b887d33121ff95511288b8d5f268ad904ff37
amorison <amorison@users.noreply.github.com>
2015-07-31 23:09:21 +0200

    add -f option to makelinks.sh
    -f option allow to force linking

--- makelinks.sh
+++ makelinks.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+forceflag=
+while getopts f name
+do
+    case $name in
+        f)    forceflag='-f';;
+        ?)    echo '-f to force'
+              exit 1;;
+    esac
+done
+
 # vim
-ln -s $PWD/vimrc ~/.vimrc
+ln -s $forceflag $PWD/vimrc ~/.vimrc
