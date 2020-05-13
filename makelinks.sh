3300e1f99ec37ac0e33718a8f876078c4140bb4c
Adrien Morison <adrien.morison@gmail.com>
2020-05-13 18:54:29 +0200

    Automatic Powerlevel10k management

--- makelinks.sh
+++ makelinks.sh
@@ -28,7 +28,25 @@ $linkcmd $PWD/vimrc ~/.vimrc
 # zsh
 $linkcmd $PWD/zshrc ~/.zshrc
 
-# p10k
+# Powerlevel10k zsh theme
+# One check if it was installed with
+#   pacman -S zsh-theme-powerlevel10k
+# otherwise, clone/update it
+p10ktheme=/usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+if [ ! -f "$p10ktheme" ]; then
+    p10kdir=$PWD/.p10ktheme
+    if [ -d "$p10kdir" ]; then
+        echo 'Update Powerlevel10k clone'
+        git -C "$p10kdir" pull
+    else
+        echo 'Clone Powerlevel10k repo'
+        p10krepo=https://github.com/romkatv/powerlevel10k.git
+        git clone --depth=1 $p10krepo "$p10kdir"
+    fi
+    p10ktheme=$p10kdir/powerlevel10k.zsh-theme
+fi
+$linkcmd $p10ktheme ~/.p10ktheme
+# personal configuration
 $linkcmd $PWD/p10k.zsh ~/.p10k.zsh
 
 # my zsh functions
--- zshrc
+++ zshrc
@@ -42,7 +42,7 @@ source ~/.alias
 
 unfunction safesource
 
-source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+source ~/.p10ktheme
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
