2ce5b9617a455babc3054e8bc38d07b787fdc2f4
Adrien Morison <adrien.morison@gmail.com>
2023-01-27 22:20:55 +0000

    nvim: display tabs and trailing spaces

--- nvim.lua
+++ nvim.lua
@@ -9,6 +9,9 @@ vim.o.expandtab = true
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 
+vim.o.list = true
+vim.o.listchars = "tab:•·❯,trail:•"
+
 vim.keymap.set('n', '<Space>', ':nohlsearch<Bar>:echo<CR>')
 
 vim.g.loaded_python3_provider = 0
