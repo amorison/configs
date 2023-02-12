1cedd9c0e016f925bc68a3d093013360e71a9e2a
Adrien Morison <adrien.morison@gmail.com>
2023-02-12 14:01:29 +0000

    nvim: mv lsp and cmp config to nvim_plugins

--- nvim_plugins.lua
+++ nvim_plugins.lua
@@ -1,3 +1,118 @@
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and
+        vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):
+        match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+    vim.api.nvim_feedkeys(
+        vim.api.nvim_replace_termcodes(key, true, true, true),
+        mode,
+        true)
+end
+
+local function cmp_config()
+    local cmp = require('cmp')
+    local cmp_types = require("cmp.types")
+
+    cmp.setup({
+        preselect = cmp_types.cmp.PreselectMode.None,
+        snippet = {
+            expand = function(args) vim.fn["vsnip#anonymous"](args.body) end,
+        },
+        mapping = cmp.mapping.preset.insert({
+            ['<C-b>'] = cmp.mapping.scroll_docs( -4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.abort(),
+            ['<CR>'] = cmp.mapping.confirm({ select = false }),
+            ["<Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                elseif vim.fn["vsnip#available"](1) == 1 then
+                    feedkey("<Plug>(vsnip-expand-or-jump)", "")
+                elseif has_words_before() then
+                    cmp.complete()
+                else
+                    fallback()
+                end
+            end, { "i", "s" }),
+
+            ["<S-Tab>"] = cmp.mapping(function()
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                elseif vim.fn["vsnip#jumpable"]( -1) == 1 then
+                    feedkey("<Plug>(vsnip-jump-prev)", "")
+                end
+            end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources(
+            { { name = 'nvim_lsp' }, { name = 'vsnip' } },
+            { { name = 'nvim_lsp_signature_help' }, { name = 'buffer' } })
+    })
+
+    cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = { { name = 'buffer' } },
+    })
+
+    cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({ { name = 'path' } },
+            { { name = 'cmdline' } })
+    })
+end
+
+local function lsp_config()
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+    local lspconfig = require('lspconfig')
+
+    lspconfig.lua_ls.setup {
+        capabilities = capabilities,
+        settings = {
+            Lua = {
+                runtime = { version = 'LuaJIT' },
+                diagnostics = { globals = { 'vim' } },
+                workspace = {
+                    library = vim.api.nvim_get_runtime_file("", true),
+                    checkThirdParty = false,
+                },
+                telemetry = { enable = false, },
+            },
+        },
+    }
+
+    lspconfig.rust_analyzer.setup { capabilities = capabilities, }
+
+    lspconfig.cmake.setup { capabilities = capabilities, }
+
+    lspconfig.clangd.setup { capabilities = capabilities }
+
+    lspconfig.fortls.setup {
+        capabilities = capabilities,
+        cmd = {
+            "fortls", "--notify_init", "--hover_signature",
+            "--hover_language=fortran", "--use_signature_help",
+            "--lowercase_intrinsics",
+        },
+    }
+
+    lspconfig.pylsp.setup {
+        capabilities = capabilities,
+        settings = {
+            pylsp = {
+                plugins = {
+                    pycodestyle = { maxLineLength = 100 },
+                },
+            },
+        },
+    }
+
+    lspconfig.texlab.setup { capabilities = capabilities }
+end
+
 return {
     {
         "rebelot/kanagawa.nvim",
@@ -41,7 +156,10 @@ return {
             vim.o.showtabline = 1
         end,
     },
-    "neovim/nvim-lspconfig",
+    {
+        "neovim/nvim-lspconfig",
+        config = lsp_config,
+    },
     {
         "j-hui/fidget.nvim",
         config = true,
@@ -67,6 +185,7 @@ return {
             "hrsh7th/cmp-vsnip",
             "hrsh7th/vim-vsnip",
         },
+        config = cmp_config,
     },
     {
         "glacambre/firenvim",
